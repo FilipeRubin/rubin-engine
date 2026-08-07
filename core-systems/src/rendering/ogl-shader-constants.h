8a5ed0f36c1cfaef0141cbf92538640f34013ed5
Filipe Rubin <filiperubin@gmail.com>
2026-08-07 17:13:57 -0300

    Add OGLShaderConstants

---
+++ core-systems/src/rendering/ogl-shader-constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace OGLShaderConstants
+{
+    constexpr const char* MODEL = "u_model";
+    constexpr const char* VIEW = "u_view";
+    constexpr const char* PROJECTION = "u_projection";
+    constexpr const char* DIR_LIGHT_AMBIENT = "u_ambientLight";
+    constexpr const char* DIR_LIGHT_DIFFUSE = "u_directionalLightDiffuse";
+    constexpr const char* DIR_LIGHT_DIRECTION = "u_directionalLightDirection";
+}
--- core-systems/src/rendering/ogl-shader-program-cache.cpp
+++ core-systems/src/rendering/ogl-shader-program-cache.cpp
@@ -1,5 +1,6 @@
 #include "ogl-shader-program-cache.h"
 #include "ogl-shader-program.h"
+#include "ogl-shader-constants.h"
 #include <rendering/ogl-renderer.h>
 #include <math/matrix4x4.h>
 #include <logging/log-macros.h>
@@ -45,29 +46,30 @@ void OGLShaderProgramCache::BindOrCreate(const OGLRenderingRule& renderingRule, 
 
 void OGLShaderProgramCache::SetUniforms(const OGLRenderingRule& renderingRule)
 {
+	using namespace OGLShaderConstants;
 	const RenderingRuleDescriptor& d = renderingRule.GetDescriptor();
 	const OGLRenderParametersState& s = GetRenderer().RenderParametersState();
 
 	if (d.useDirectionalLight)
 	{
 		const DirectionalLight& dl = s.directionalLight->Light();
-		m_currentProgram->SetUniform("u_ambientLight", dl.ambient);
-		m_currentProgram->SetUniform("u_directionalLightDiffuse", dl.diffuse);
-		m_currentProgram->SetUniform("u_directionalLightDirection", dl.direction);
+		m_currentProgram->SetUniform(DIR_LIGHT_AMBIENT, dl.ambient);
+		m_currentProgram->SetUniform(DIR_LIGHT_DIFFUSE, dl.diffuse);
+		m_currentProgram->SetUniform(DIR_LIGHT_DIRECTION, dl.direction);
 	}
 	if (d.useCamera3D)
 	{
 		const Camera3D& c = s.camera->Camera();
 		Matrix4x4 view = Matrix4x4::View(-c.position, -c.rotation);
 		Matrix4x4 projection = Matrix4x4::Perspective(c.aspectRatio, c.vFOV, c.zNear, c.zFar);
-		m_currentProgram->SetUniform("u_view", view);
-		m_currentProgram->SetUniform("u_projection", projection);
+		m_currentProgram->SetUniform(VIEW, view);
+		m_currentProgram->SetUniform(PROJECTION, projection);
 	}
 	if (d.useTransform3D)
 	{
 		const Transform3D& t = s.transform->Transform();
 		Matrix4x4 model = Matrix4x4::Model(t.position, t.rotation, t.scale);
-		m_currentProgram->SetUniform("u_model", model);
+		m_currentProgram->SetUniform(MODEL, model);
 	}
 }
 
@@ -92,6 +94,5 @@ OGLShaderProgram* OGLShaderProgramCache::CreateProgram(const OGLShaderKey& key)
 		LOG_DEBUG("OpenGL shader program compiled and linked.");
 		return program;
 	}
-	LOG_ERROR("Failed to create shader program.");
 	return nullptr;
 }
